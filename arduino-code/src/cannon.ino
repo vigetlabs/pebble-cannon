6de8f96b203e2bb415bafe6ea9ac61e87b2359da
Eli Fatsi <eli.fatsi@viget.com>
2015-09-05 05:15:31 -0600

    Added servo stuff

--- arduino-code/src/cannon.ino
+++ arduino-code/src/cannon.ino
@@ -1,12 +1,27 @@
 #include <ArduinoPebbleSerial.h>
+#include <Servo.h>
 
 static const uint16_t SUPPORTED_SERVICES[] = {0x1001};
 static const uint8_t NUM_SERVICES = 2;
 // static uint8_t pebble_buffer[GET_PAYLOAD_BUFFER_SIZE(200)];
 static uint8_t pebble_buffer[256];
+static bool is_connected = false;
+
+Servo xAxisServo;
+Servo yAxisServo;
+Servo triggerServo;
+
+bool xRead       = false;
+bool zRead       = false;
+bool yRead       = false;
+bool bearingRead = false;
+
+int16_t xAxis;
+int16_t yAxis;
+int16_t zAxis;
+int16_t bearing;
 
 void setup() {
-  // General init
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
@@ -20,17 +35,38 @@ void setup() {
   // Teensy 3.0/3.1 uses hardware serial mode (pins 0/1) with RX/TX shorted together
   ArduinoPebbleSerial::begin_hardware(pebble_buffer, sizeof(pebble_buffer), Baud57600,
                                       SUPPORTED_SERVICES, NUM_SERVICES);
+
+  xAxisServo.attach(3);
+  yAxisServo.attach(4);
+  triggerServo.attach(5);
 }
 
-bool xRead = false;
-bool zRead = false;
-bool yRead = false;
-
-int16_t xAxis;
-int16_t yAxis;
-int16_t zAxis;
-
 void loop() {
+  determineState();
+  display();
+  // servoMessingAround();
+}
+
+void determineState() {
+  checkForConnection();
+  if (is_connected) readFromPebble();
+}
+
+void checkForConnection() {
+  if (ArduinoPebbleSerial::is_connected()) {
+    if (!is_connected) {
+      Serial.println("Connected to the smartstrap!");
+      is_connected = true;
+    }
+  } else {
+    if (is_connected) {
+      Serial.println("Disconnected from the smartstrap!");
+      is_connected = false;
+    }
+  }
+}
+
+void readFromPebble() {
   // Let the ArduinoPebbleSerial code do its processing
   size_t length;
   uint16_t service_id;
@@ -51,29 +87,36 @@ void loop() {
         zAxis = *(int16_t*)pebble_buffer;
         zRead = true;
       }
-      else if (attribute_id == 0x1004) {
-        Serial.println("TAPP!");
+      else if (attribute_id == 0x1005) {
+        bearing = *(int16_t*)pebble_buffer;
+        bearingRead = true;
       }
     }
   }
+}
 
-  if (xRead && yRead && zRead) {
-    xRead = false;
-    zRead = false;
-    yRead = false;
-    Serial.printf("%d\t%d\t%d\n", xAxis, yAxis, zAxis);
+void display() {
+  if (xRead && yRead && zRead && bearingRead) {
+    xRead       = false;
+    zRead       = false;
+    yRead       = false;
+    bearingRead = false;
+    Serial.printf("%d\t%d\t%d\t%d\n", xAxis, yAxis, zAxis, bearing);
   }
+}
 
-  static bool is_connected = false;
-  if (ArduinoPebbleSerial::is_connected()) {
-    if (!is_connected) {
-      Serial.println("Connected to the smartstrap!");
-      is_connected = true;
-    }
-  } else {
-    if (is_connected) {
-      Serial.println("Disconnected from the smartstrap!");
-      is_connected = false;
-    }
+void servoMessingAround() {
+  int pos;
+  for(pos = 10; pos < 170; pos += 1) {
+    xAxisServo.write(pos);
+    yAxisServo.write(pos);
+    triggerServo.write(pos);
+    delay(15);
+  }
+  for(pos = 180; pos>=1; pos-=1) {
+    xAxisServo.write(pos);
+    yAxisServo.write(pos);
+    triggerServo.write(pos);
+    delay(15);
   }
 }
